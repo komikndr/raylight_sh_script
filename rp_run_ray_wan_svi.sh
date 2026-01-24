4d7ee0671fb13a32f5f65e03ecb6190e6deea3fa
komikndr <mickolesmana@gmail.com>
2026-01-24 16:19:33 +0700

    adding SVI

---
+++ rp_run_ray_wan_svi.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+mkdir -p /workspace
+
+if [ ! -d "/workspace/ComfyUI" ]; then
+  git clone https://github.com/comfyanonymous/ComfyUI /workspace/ComfyUI
+  pip install -r /workspace/ComfyUI/requirements.txt
+fi
+
+if [ ! -d "/workspace/ComfyUI/custom_nodes/raylight" ]; then
+  git clone https://github.com/komikndr/raylight /workspace/ComfyUI/custom_nodes/raylight
+  pip install -r /workspace/ComfyUI/custom_nodes/raylight/requirements.txt
+fi
+
+if [ ! -d "/workspace/ComfyUI/custom_nodes/ComfyUI-Easy-Use" ]; then
+  git clone https://github.com/yolain/ComfyUI-Easy-Use /workspace/ComfyUI/custom_nodes/ComfyUI-Easy-Use
+  pip install -r /workspace/ComfyUI/custom_nodes/ComfyUI-Easy-Use/requirements.txt
+fi
+
+mkdir -p /workspace/ComfyUI/models/diffusion_models \
+         /workspace/ComfyUI/models/text_encoders \
+         /workspace/ComfyUI/models/vae
+
+wget -O /workspace/ComfyUI/models/diffusion_models/Wan2_2-I2V-A14B-HIGH_fp8_e4m3fn_scaled_KJ.safetensors \
+  "https://huggingface.co/Kijai/WanVideo_comfy_fp8_scaled/resolve/main/I2V/Wan2_2-I2V-A14B-HIGH_fp8_e4m3fn_scaled_KJ.safetensors?download=true" &
+
+wget -O /workspace/ComfyUI/models/diffusion_models/Wan2_2-I2V-A14B-LOW_fp8_e4m3fn_scaled_KJ.safetensors \
+  "https://huggingface.co/Kijai/WanVideo_comfy_fp8_scaled/resolve/main/I2V/Wan2_2-I2V-A14B-LOW_fp8_e4m3fn_scaled_KJ.safetensors?download=true" &
+
+wget -O /workspace/ComfyUI/models/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors \
+  "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors?download=true" &
+
+wget -O /workspace/ComfyUI/models/vae/wan_2.1_vae.safetensors \
+  "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors?download=true" &
+
+wget -O /workspace/ComfyUI/models/loras/lightx2v_I2V_14B_480p_cfg_step_distill_rank128_bf16.safetensors \
+  "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Lightx2v/lightx2v_I2V_14B_480p_cfg_step_distill_rank128_bf16.safetensors?download=true" &
+
+wget -O /workspace/ComfyUI/models/loras/Wan_2_2_I2V_A14B_HIGH_lightx2v_4step_lora_v1030_rank_64_bf16.safetensors \
+  "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/LoRAs/Wan22_Lightx2v/Wan_2_2_I2V_A14B_HIGH_lightx2v_4step_lora_v1030_rank_64_bf16.safetensors?download=true" &
+
+wget -O /workspace/ComfyUI/models/loras/SVI_v2_PRO_Wan2.2-I2V-A14B_HIGH_lora_rank_128_fp16.safetensors \
+  "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/LoRAs/Stable-Video-Infinity/v2.0/SVI_v2_PRO_Wan2.2-I2V-A14B_HIGH_lora_rank_128_fp16.safetensors?download=true" &
+
+wget -O /workspace/ComfyUI/models/loras/SVI_v2_PRO_Wan2.2-I2V-A14B_LOW_lora_rank_128_fp16.safetensors \
+  "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/LoRAs/Stable-Video-Infinity/v2.0/SVI_v2_PRO_Wan2.2-I2V-A14B_LOW_lora_rank_128_fp16.safetensors?download=true" &
+
+wait
